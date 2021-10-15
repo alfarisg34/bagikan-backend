9d280713c271416f608e0f2f2e2412b4ddc9f551
Alfarisg 34 <alfarisg34@gmail.com>
2021-10-15 09:47:27 +0700

    Revert "add validation model"
    This reverts commit f691a041039a91caa10e2787262c8cbcb52bdf47.

--- models/Admin.js
+++ models/Admin.js
@@ -5,11 +5,8 @@ const Scheme = mongoose.Schema;
 const AdminScheme = new Scheme({
     username:{
         type:String,
-        required: [true,'Username field is required'],
-        unique:true,
-        lowercase: true,
-        minlength: [6, 'Username can not be less than 6 characters'],
-        maxlength: [15, 'Username can not be more than 15 characters'],
+        required: [true,'adminname field is required'],
+        unique:true
     },
     password: {
         type: String,
--- models/User.js
+++ models/User.js
@@ -6,21 +6,15 @@ const UserScheme = new Scheme({
     username:{
         type:String,
         required: [true,'Username field is required'],
-        unique:true,
-        lowercase: true,
-        minlength: [6, 'Username can not be less than 6 characters'],
-        maxlength: [15, 'Username can not be more than 15 characters'],
+        unique:true
     },
     email: {
         type: String,
         required: [true,'Email field is required'],
-        unique:true,
-        validate: [isEmail, 'Please enter a valid email'],
+        unique:true
     },
     password: {
         type: String,
-        minlength: [6, 'Password can not be less than 6 characters'],
-        maxlength: [15, 'Password can not be more than 15 characters'],
         required: [true,'Password field is required'],
     },
     nama : {
@@ -44,11 +38,5 @@ const UserScheme = new Scheme({
     }
 });
 
-UserSchema.pre('save', async function (next) {
-    const hashedPassword = await bcrypt.hash(this.password, 12)
-    this.password = hashedPassword
-    next()
-  })
-  
 const User = mongoose.model('user',UserScheme);
 module.exports = User;
