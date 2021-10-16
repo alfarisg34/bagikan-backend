056759a39c8a9c116087b4af65f7eca76f42120d
Alfarisg 34 <alfarisg34@gmail.com>
2021-10-16 11:18:48 +0700

    validation

--- validation/registerValidation.js
+++ validation/registerValidation.js
@@ -1,38 +1,33 @@
 const validator = require("validator");
 const isEmpty = require("./is-emptyValidation");
 
-module.exports = function validateRegisterInput(data){
+module.exports = function registerValidation(data){
     let errors = {};
 
-    data.name = !isEmpty(data.name) ? data.name:"" ; 
+    data.username = !isEmpty(data.username) ? data.username:"" ; 
     data.email = !isEmpty(data.email) ? data.email:"" ; 
     data.password = !isEmpty(data.password) ? data.password:"" ; 
-    data.password2 = !isEmpty(data.password2) ? data.password2:"" ; 
+    // data.password2 = !isEmpty(data.password2) ? data.password2:"" ; 
 
-    if(!validator.isLength(data.username,{min:3,max:50})){
-        errors.name = "Nama Harus diantara 3 dan 50 Karakter";
+    if(!validator.isLength(data.username,{min:6,max:16})){
+        errors.name = "Nama Harus diantara 6 dan 16 Karakter";
     }
-
-    if(!validator.isLength(data.password,{min:6,max:50})){
+    if(!validator.isLength(data.password,{min:6,max:24})){
         errors.password = "Password minimal 6 karakter";
     }
-
-
     if(validator.isEmpty(data.username)){
         errors.name = "Data nama dibutuhkan";
-    }
-    
+    }    
     if(validator.isEmpty(data.email)){
         errors.email = "Data Email dibutuhkan";
     }
-
+    if(validator.isEmpty(data.password)){
+        errors.password = "Data Password dibutuhkan";
+    }  
     if(!validator.isEmail(data.email)){
         errors.email = "Email tidak valid";
     }
 
-    if(validator.isEmpty(data.password)){
-        errors.password = "Data Password dibutuhkan";
-    }   
 
     // if(!validator.equals(data.password2,data.password)){
     //     errors.password2 = "Data password dan confirmed password harus sama";
