d7b43961102f659e009891366b57e7523b1270b3
Alfarisg 34 <alfarisg34@gmail.com>
2021-10-16 16:08:47 +0700

    seeder

---
+++ src/api/v1/seeders/data/1-users/user.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    username: 'user001',
+    email: 'user001@gmail.com',
+    password: 'useruser',
+  },
+  {
+    username: 'user002',
+    email: 'user002@gmail.com',
+    password: 'useruser',
+  },
+  {
+    username: 'user003',
+    email: 'user003@gmail.com',
+    password: 'useruser',
+  },
+]
---
+++ src/api/v1/seeders/data/2-admins/admin.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {
+      username: 'admin001',
+      password: 'adminadmin',
+    },
+    {
+      username: 'admin002',
+      password: 'adminadmin',
+    },
+    {
+      username: 'admin003',
+      password: 'adminadmin',
+    },
+  ]
+  
