51f198fb6942f65bd97619363f97ee83931f64cb
Alfarisg 34 <alfarisg34@gmail.com>
2021-10-17 20:23:57 +0700

    get all user - admin

--- src/api/v1/routes/adminRoutes.js
+++ src/api/v1/routes/adminRoutes.js
@@ -5,5 +5,6 @@ const router = Router()
 
 router.post('/login/admin', adminController.login)
 router.post('/token', authController.refreshToken)
+router.post('/delete/user', adminController.deleteUser)
 
 module.exports = router
