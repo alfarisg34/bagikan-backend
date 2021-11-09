cb9c570094ab214158145f3bd743db2e4bb554e4
Alfarisg 34 <alfarisg34@gmail.com>
2021-11-09 23:03:59 +0700

    change database config format

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose')
 
 module.exports = async (next) => {
   try {
-    await mongoose.connect(
+    const connectionString =
+      process.env.DB_URI ||
       `mongodb://localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`
-    )
+
+    await mongoose.connect(connectionString)
     console.log(`Database ${process.env.DB_NAME} is now connected!`)
     next()
   } catch (err) {
